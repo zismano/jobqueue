e74785634d7d0b3d71eb308c97c538d33d1cd1b4
Ofir Zisman <zismanofir@gmail.com>
2018-05-17 14:34:10 -0700

    Add express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let path = require('path');
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(3000);
