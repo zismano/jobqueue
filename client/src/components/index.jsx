5f00727f856d5bc88be1236a45c0e83ab3d219df
Ofir Zisman <zismanofir@gmail.com>
2018-05-17 18:21:09 -0700

    Add axios request to post url from user Add dangerouslySetInnerHTML attribute to render html body of url

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import FetchUrl from './fetchUrl.jsx';
+
+const axios = require('axios');
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			url: ''
 		}
+		this.fetchUrlFromUser = this.fetchUrlFromUser.bind(this);
+	}
+
+	createMarkup() {
+		return {__html: this.state.url}
+	}
+
+	fetchUrlFromUser(url) {
+		axios.post(`/url/:${url}`, {
+			params: {
+				url,			
+			},
+		})
+		.then(res => this.setState({ url: res.data }))
+
 	}
 
 	render() {
 		return (
+			this.state.url ? (
+			<div dangerouslySetInnerHTML={this.createMarkup()}></div>
+			) : (
 			<div>
-			<h1>Welcome to Job Queue</h1>
-			<h2>Please enter a url:</h2>
-			<form>
-				<input type="text" placeholder="www.google.com"></input>
-				<button>Submit</button>
-			</form>
-			</div>
+				<h1>Welcome to Job Queue</h1>
+				<h2>Please enter a url:</h2>
+				<FetchUrl FetchUrl={this.fetchUrlFromUser} />
+{/*// {/*			<CheckStatus />
+*/}			</div>
+			 )
 		)
 	}
 }
