dd43596bbe5ea9559e001bea6e1b60b813bd3e03
Ofir Zisman <zismanofir@gmail.com>
2018-05-17 18:23:04 -0700

    Add endpoint to fetch user url and send html body of url in response

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-let path = require('path');
+const path = require('path');
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+const http = require('http');
+
+app.post('/url/:url', (req, res) => {
+	let url = req.params.url.split(':')[1];
+	http.get({ host: url }, response => {
+		let body = '';
+		response.on('data', chunk => {
+			body += chunk;
+		})
+		response.on('end', () => {
+		  res.send(body);
+		})
+	})
+})
 
 app.listen(3000);
