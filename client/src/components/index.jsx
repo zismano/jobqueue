093441e973447c91f3d37fd23551a248c0662f8c
Ofir Zisman <zismanofir@gmail.com>
2018-05-19 08:16:45 -0700

    implement checking for validity of url functionality

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -9,7 +9,8 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			url: ''
+			url: '',
+			id: ''
 		}
 		this.fetchUrlFromUser = this.fetchUrlFromUser.bind(this);
 	}
@@ -19,13 +20,32 @@ class App extends React.Component {
 	}
 
 	fetchUrlFromUser(url) {
+		if (this.isValidUrl) {
+			this.getIdForUrl(url);
+		} else {
+			console.log('not valid url')
+		}
+	}
+
+	isValidUrl(url) {	// returns true if url has 2-3 dotts in name and no spaces
+		let splittledUrl = url.split(' ');
+		if (splittedUrl.length === 1) {
+			let splittedUrl = url.split('.');
+			if (splittedUrl.length > 1 && splittedUrl.length < 4) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	getIdForUrl(url) {
 		axios.post(`/url/:${url}`, {
 			params: {
 				url,			
 			},
 		})
-		.then(res => this.setState({ url: res.data }))
-
+		.then(id => this.setState({ url, id }))
+		.catch(err => { throw err });			
 	}
 
 	render() {
