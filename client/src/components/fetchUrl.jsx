a2b8b58e7616b21b6f87fdf23700d9470a6d7f1a
Ofir Zisman <zismanofir@gmail.com>
2018-05-19 11:26:52 -0700

    add condition

--- client/src/components/fetchUrl.jsx
+++ client/src/components/fetchUrl.jsx
@@ -1,19 +1,26 @@
 import React from 'React';
 
-let FetchUrl = props => {
-	let url;
+const FetchUrl = props => {
+	let inputUrl;
+	let { url, id } = props.currJob;
 	return (
 		<div>
 			<form>
 				<input type="text" 
 					   placeholder="www.google.com" 
-					   ref={input => url = input}>
+					   ref={input => inputUrl = input}>
 				</input>
 				<button onClick={(e) => {
 					e.preventDefault();
-					props.FetchUrl(url.value);
-				}}>Submit</button>
+					props.FetchUrl(inputUrl.value);
+					}}>Submit
+				</button>
 			</form>
+			{ url !== '' ? (
+			  	<div>{url} added to queue with job id {id}</div>
+			  ) : (
+			  	<div></div>
+			  ) }
 		</div>
 	);
 }
