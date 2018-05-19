6d7273f6fc4400e02a0311dca8b5f560914d089e
Ofir Zisman <zismanofir@gmail.com>
2018-05-18 22:16:43 -0700

    add api endpoint for getting job id from user

--- server/index.js
+++ server/index.js
@@ -7,33 +7,62 @@ app.use(bodyParser.json());
 const path = require('path');
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-const http = require('http');
-const https = require('https');
+const db = require('../database/index.js');
+
+const jobQueue = require('../helpers/jobQueue.js');
+
+
+// app.get('/url/:url', (req, res) => {
+// 	let url = req.params.url.split(':')[1];
+// 	http.get({ host: url }, response => {
+// 		let body = '';
+// 		response.on('data', chunk => {
+// 			body += chunk;
+// 		})
+// 		response.on('end', () => {
+// 		  if (body === '') {
+// 		  	https.get({ host: url }, response => {
+// 		  		let body = '';
+// 				response.on('data', chunk => {
+// 					body += chunk;
+// 				})
+// 				response.on('end', () => {		  		
+// 					res.send(body);
+// 				})
+// 			})
+// 		  } else {
+// 			  res.send(body);		  	
+// 		  }
+// 		})
+// 	})
+// })
 
 app.post('/url/:url', (req, res) => {
-	let url = req.params.url.split(':')[1];
-
-	http.get({ host: url }, response => {
-		let body = '';
-		response.on('data', chunk => {
-			body += chunk;
-		})
-		response.on('end', () => {
-		  if (body === '') {
-		  	https.get({ host: url }, response => {
-		  		let body = '';
-				response.on('data', chunk => {
-					body += chunk;
-				})
-				response.on('end', () => {		  		
-					res.send(body);
-				})
+	let url = req.params.url.split(':')[1];	// extract url from request
+	// check if url already has id
+	db.doesUrlHaveJobId(url, id => {
+		if (id) {
+			res.send('Job has id!');			
+		} else {
+			jobQueue.addJobToQueue(url, (id) => {
+				res.send(`url ${url} got id ${id}`);							
 			})
-		  } else {
-			  res.send(body);		  	
-		  }
-		})
+		}
 	})
-})
+});
+
+app.get('/id/:id', (req, res) => {
+	let id = req.params.id.split(':')[1];
+	// check html of id in db
+	db.findHTMLOfId(id, data => {
+		if (data) {
+			res.send(data);
+		} else {
+			res.send('Job is not finished yet');
+		}
+	})
+	// keep track of id (check if user put something else than number)
+});
+
 
 app.listen(3000);
