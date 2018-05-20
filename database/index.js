bc0945e7ca7bb24f46cdf4dab059b0d9deddebcb
Ofir Zisman <zismanofir@gmail.com>
2018-05-20 09:47:27 -0700

    refactor to connection pool

--- database/index.js
+++ database/index.js
@@ -1,90 +1,59 @@
-const MongoClient = require('mongodb').MongoClient;
- 
-// Connection URL
-const urlMongoDB = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'jobqueue';
- 
-// Use connect method to connect to the server
-const doesUrlHaveJobId = (urlFromUser, cb) => {
-	MongoClient.connect(urlMongoDB, function(err, client) {
-	  if (err) throw err;	
-	  console.log("Connected successfully to server");
-	 
-	  const jobsDB = client.db(dbName);
+var MongoClient = require('mongodb').MongoClient;
+let jobsDB;
 
-	  jobsDB.collection("urls").findOne({"url": urlFromUser})
-	  .then(doc => {
-	  	if (!doc) {
-	  		cb(null);
-	  	} else {
-	  		cb(doc.jobId);
-	  	}
-	  })
-	  .catch(err => { throw err })
-	});	
+MongoClient.connect("mongodb://localhost:27017", function(err, client) {
+	jobsDB = client.db('jobqueue');
+})
+
+const doesUrlHaveJobId = (urlFromUser, cb) => {
+	jobsDB.collection("urls").findOne({"url": urlFromUser})
+	.then(doc => {
+		if (!doc) {
+			cb(null);
+		} else {
+			cb(doc.jobId);
+		}
+	})
+	.catch(err => { throw err })
 }
 
 const findHTMLOfId = (id, cb) => {
-	MongoClient.connect(urlMongoDB, function(err, client) {
-	  if (err) throw err;
-	  console.log("Connected successfully to server");
-	 
-	  const jobsDB = client.db(dbName);
-
-	  jobsDB.collection("urls").findOne({"jobId": id})
-	  .then(doc => {
-	  	if (doc) {
-	  		cb(doc);
-	  	} else {
-	  		cb('No id was found');
-	  	}
-	  })
-	  .catch(err => {
-	  	throw err;
-	  })
-	});		
+	jobsDB.collection("urls").findOne({"jobId": id})
+	.then(doc => {
+		if (doc) {
+			cb(doc);
+		} else {
+			cb('No id was found');
+		}
+	})
+	.catch(err => {
+		throw err;
+	})
 }
 
 const addHTMLtoId = (id, url, data, cb) => {
-	MongoClient.connect(urlMongoDB, function(err, client) {
-	  if (err) throw err;
-	  console.log("Connected successfully to server");
-	 
-	  const jobsDB = client.db(dbName);
-
-	  const row = {
-	  	jobId: id,
-	  	url,
-	  	data,
-	  }
-
-	  jobsDB.collection("urls").insertOne(row, (err, res) => {
-	  	if (err) {
-	  		cb(err);
-	  	} else {
-	  		cb(null, res);
-	  	}
-	  })
-	});			
+	const row = {
+		jobId: id,
+		url,
+		data,
+	}
+	jobsDB.collection("urls").insertOne(row, (err, res) => {
+		if (err) {
+			cb(err);
+		} else {
+			cb(null, res);
+		}
+	})
 }
 
 const findAllJobs = cb => {
-	MongoClient.connect(urlMongoDB, function(err, client) {
-	  if (err) throw err;
-	  console.log("Connected successfully to server");
-	 
-	  const jobsDB = client.db(dbName);
-
-	  jobsDB.collection("urls").find({}).toArray((err, res) => {
-	  	if (err) {
-	  		cb(err);
-	  	} else {
-	  		cb(null, res);
-	  	}
-	  })
-	});		
+	jobsDB.collection("urls").find({}).toArray((err, res) => {
+		if (err) {
+			cb(err);
+		} else {
+			cb(null, res);
+		}
+	})
 }
 
 module.exports = {
