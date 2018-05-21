e1319f33c161e5b386e4245c54777dfb10bc6fe2
Ofir Zisman <zismanofir@gmail.com>
2018-05-20 21:28:13 -0700

    remove check url validity function

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,14 +1,3 @@
-const isValidUrl = url => {	// very basic check, returns true if url has 1-3 dotts in name and no spaces
-	let splittedUrl = url.split(' ');
-	if (splittedUrl.length === 1) {
-		let splittedUrl = url.split('.');
-		if (splittedUrl.length > 1 && splittedUrl.length < 5) {
-			return true;
-		}
-	}
-	return false;
-}
-
 const isValidId = (id, jobs) => {		// checks if id entered is valid. Assumption: last element in allJobs state is highest id
 	if (jobs.length && 
 		id > 0 &&
@@ -20,6 +9,5 @@ const isValidId = (id, jobs) => {		// checks if id entered is valid. Assumption:
 }
 
 module.exports = {
-	isValidUrl,
 	isValidId
 }
